8a34192e6546f2b0c9e6b7ca833dd8d59ab26e0b
daedela <97813898+daedela@users.noreply.github.com>
2022-01-21 21:05:52 -0800

    Update skills.c

--- src/skills.c
+++ src/skills.c
@@ -1,3 +1,15 @@
+/***************************************************************************
+ *  File: skills.c                                       Part of FieryMUD  *
+ *  Usage: Skill-management functions and data                             *
+ *                                                                         *
+ *  All rights reserved.  See license.doc for complete information.        *
+ *                                                                         *
+ *  FieryMUD Copyright (C) 1998, 1999, 2000 by the Fiery Consortium        *
+ *  FieryMUD is based on CircleMUD Copyright (C) 1993, 94 by the Trustees  *
+ *  of the Johns Hopkins University                                        *
+ *  CircleMUD is based on DikuMUD, Copyright (C) 1990, 1991.               *
+ ***************************************************************************/
+
 #include "skills.h"
 
 #include "casting.h"
