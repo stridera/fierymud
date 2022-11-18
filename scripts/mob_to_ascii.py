ee55c0ef3ae7b0448b7cdeae4a8be5f605f2af71
Matt Jones <stridera@gmail.com>
2022-11-18 03:07:53 -0800

    save

--- scripts/mob_to_ascii.py
+++ scripts/mob_to_ascii.py
@@ -1,13 +1,28 @@
+import argparse
 
 
+def process_file(filename, vnum=-1):
+    print(f"Processing {filename}")
+    with open(filename) as f:
+        for line in f:
+            if line.startswith('#'):
+                print(line, end='')
 
 
+def process_index(index_file, vnum):
+    path = index_file[:-5]
+    with open(index_file) as f:
+        for line in f:
+            if line.startswith('$'):
+                print('End of index')
+                return
+            else:
+                process_file(path + line.rstrip())
 
 
-
-def main(mob_file, vnum, output_file):
+def main(mob_file, vnum):
     if mob_file.endswith('index'):
-        process_index(mob_file, vnum, output_file)
+        process_index(mob_file, vnum)
     else:
         process_file(mob_file, vnum)
 
@@ -15,10 +30,8 @@ def main(mob_file, vnum, output_file):
 if __name__ == '__main__':
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Converts a mob file to use ascii flags.')
-    parser.add_argument('mob_file', help='The mob file to convert or index file to convert all.', default='lib/')
-    parser.add_argument('-n', '--number', help='The mob number to convert.', type=int)
-    parser.add_argument('-v', '--verbose', help='Verbose output.', action='store_true')
+    parser.add_argument('--file', help='The mob file to convert or index file to convert all.',
+                        type=str, default='lib/world/mob/index')
+    parser.add_argument('-n', '--number', help='The mob number to convert.', type=int, default=-1)
     args = parser.parse_args()
-    main(args.mob_file, args.number, args.verbose)
-
-    
+    main(args.file, args.number)
